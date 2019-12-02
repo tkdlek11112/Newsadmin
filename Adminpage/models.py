2c9a401912b192ef3de90b0a3eb383e0a406ce20
Cho <tkdlek11112@gmail.com>
2019-12-02 20:24:47 +0900

    real_news_data

--- Adminpage/models.py
+++ Adminpage/models.py
@@ -18,3 +18,19 @@ class LearnNews(models.Model):
     target = models.IntegerField(default=0)
 
     objects = LearnNewsManager()
+
+
+class RealNewsManager(models.Manager):
+    def create_news(self, date, time, title, tokens, target):
+        real_news = self.create(date=date, time=time, title=title, tokens=tokens, target=target)
+        return real_news
+
+
+class RealNews(models.Model):
+    date = models.DateField(default=now)
+    time = models.TimeField(default=now)
+    title = models.TextField(default='')
+    tokens = models.TextField(default='')
+    target = models.IntegerField(default=0)
+
+    objects = LearnNewsManager()
--- Adminpage/views.py
+++ Adminpage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import LearnNews
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from .models import LearnNews, RealNews
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
 # Create your views here.
@@ -20,9 +22,75 @@ def list_learn_news(request):
     else:
         search_type = '1'
         search_keyword = ''
-        news_list = LearnNews.objects.all()
+        news_list = LearnNews.objects.all().order_by('pk')
 
     paginator = Paginator(news_list, 20)  # "5" 한페이지에서 보여줄 갯수를 정한다.
     page = request.GET.get('page')
     newses = paginator.get_page(page)
     return render(request, 'adminpage/list_learn_news.html', {'newses': newses, 'type': search_type, 'keyword': search_keyword})
+
+@csrf_exempt
+def toggle_learn_news(request):
+    target = request.POST.get('target', '0')
+    pk = request.POST.get('pk', '')
+    if target == '1':
+        next_target = 0
+    else:
+        next_target = 1
+
+    update_model = LearnNews.objects.get(pk=pk)
+    update_model.target = next_target
+    update_model.save()
+
+    return HttpResponse(status=200)
+
+
+def list_real_news(request):
+    search_type = request.GET.get('search_type')
+    search_keyword = request.GET.get('search_keyword')
+    if search_keyword is None:
+        search_keyword = ""
+
+    if len(search_keyword) > 0:
+        if search_type == '1':
+            news_list = RealNews.objects.order_by('pk').filter(title__contains=search_keyword)
+        elif search_type == '2':
+            news_list = RealNews.objects.order_by('pk').filter(target__contains=search_keyword)
+
+    else:
+        search_type = '1'
+        search_keyword = ''
+        news_list = RealNews.objects.all().order_by('pk')
+
+    paginator = Paginator(news_list, 20)  # "5" 한페이지에서 보여줄 갯수를 정한다.
+    page = request.GET.get('page')
+    newses = paginator.get_page(page)
+    return render(request, 'adminpage/list_real_news.html', {'newses': newses, 'type': search_type, 'keyword': search_keyword})
+
+
+@csrf_exempt
+def toggle_real_news(request):
+    target = request.POST.get('target', '0')
+    pk = request.POST.get('pk', '')
+    if target == '1':
+        next_target = 0
+    else :
+        next_target = 1
+
+    update_model = RealNews.objects.get(pk=pk)
+    update_model.target = next_target
+    update_model.save()
+
+    return HttpResponse(status=200)
+
+@csrf_exempt
+def push_real2learn(request):
+    real_models = RealNews.objects.all().order_by('pk')
+
+    for real_model in real_models:
+        create_model = LearnNews.objects.create_news(real_model.date, real_model.time, real_model.title, real_model.tokens, real_model. target)
+        create_model.save()
+
+    real_models.delete()
+
+    return HttpResponse(status=200)
